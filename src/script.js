14207095667eeba776e1821da01d1a2d5fd55e0c
Andy Dayton <amdayton@gmail.com>
2024-12-02 17:04:53 +0100

    ok we're doing fine now

--- src/script.js
+++ src/script.js
@@ -1,11 +1,3 @@
-const slider = document.getElementById('slider')
-const btnLess = document.getElementById('btn-less')
-const btnMore = document.getElementById('btn-more')
+const ctx = window.AMD
 
-const bioElements = [
-  document.getElementById('bio1'),
-  document.getElementById('bio2'),
-  document.getElementById('bio3'),
-  document.getElementById('bio4'),
-  document.getElementById('bio5'),
-]
+console.log(ctx)
