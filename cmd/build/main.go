1ff63d7ef9e3f2c96abf128b8eb600c557d7b0e0
Andy Dayton <amdayton@gmail.com>
2024-12-02 13:37:54 +0100

    wip

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,1 +1,11 @@
 package main
+
+import amdvanilla "github.com/amd-vanilla"
+
+func main() {
+	_, err := amdvanilla.LoadContent()
+
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package amdvanilla
 
 import (
 	"embed"
+	"errors"
+	"fmt"
 	"html/template"
 )
 
@@ -21,11 +23,37 @@ type ContentMarkdown struct {
 }
 
 type Content struct {
-	Data        ContentData     `json:"data"`
-	BioXS       ContentMarkdown `json:"bio-xs"`
-	BioSM       ContentMarkdown `json:"bio-sm"`
-	BioMD       ContentMarkdown `json:"bio-md"`
-	BioLG       ContentMarkdown `json:"bio-lg"`
-	BioXL       ContentMarkdown `json:"bio-xl"`
-	ContentJSON template.JS     `json:"-"`
+	Data  ContentData     `json:"data"`
+	BioXS ContentMarkdown `json:"bio-xs"`
+	BioSM ContentMarkdown `json:"bio-sm"`
+	BioMD ContentMarkdown `json:"bio-md"`
+	BioLG ContentMarkdown `json:"bio-lg"`
+	BioXL ContentMarkdown `json:"bio-xl"`
+}
+
+type Context struct {
+	Content     Content
+	ContentJSON template.JS
+}
+
+func LoadContent() (*Content, error) {
+
+	files, err := ContentDir.ReadDir("content")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+
+	return nil, errors.New("not implemented")
+}
+
+func main() {
+	_, err := LoadContent()
+
+	if err != nil {
+		panic(err)
+	}
 }
